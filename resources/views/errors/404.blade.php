6240dc017810d27d466701239eb68b57e6980854
ivars <ivars@ivars.lv>
2019-06-03 23:52:19 +0300

    vienkārša 404 kļūdas lapa

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Kļūda</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+
+        /* Links */
+        a,
+        a:focus,
+        a:hover {
+            color: #fff;
+        }
+
+
+
+        html,
+        body {
+            height: 100%;
+            background-color: #333;
+        }
+
+        body {
+            display: -ms-flexbox;
+            display: flex;
+            color: #fff;
+            text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+            box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+        }
+
+        .cover-container {
+            max-width: 42em;
+        }
+
+
+        .cover {
+            padding: 2.5rem 1.5rem;
+        }
+        .cover .btn-lg {
+            padding: 1.75rem 1.25rem;
+            font-weight: 700;
+        }
+
+
+        /*
+         * Footer
+         */
+        .mastfoot {
+            color: rgba(255, 255, 255, .5);
+        }
+    </style>
+</head>
+<body class="text-center">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+
+    <main role="main" class="inner cover">
+        <h1 class="cover-heading">Šāda lapa neeksistē!</h1>
+        <p class="lead">
+           Šāda lapa vai šāds pieteikums neeksistē!
+        </p>
+
+    </main>
+</div>
+</body>
+</html>
