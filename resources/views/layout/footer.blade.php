c158f6b91d9d81d34cbbefa6642a91bcde7a1ce8
ivars <ivars@ivars.lv>
2019-06-03 23:58:04 +0300

    footer aplaimošana ar saiti

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,6 +2,6 @@
     <p class="mb-1">&copy; {{ date('Y') }} studentu projekts</p>
     <ul class="list-inline">
         <li class="list-inline-item"><a href="https://dif2.liepu.lv">Mācībiestāde</a></li>
-        <li class="list-inline-item"><a href="#">Publiski pieejamais kods (MIT licence)</a></li>
+        <li class="list-inline-item"><a href="https://github.com/IvarsSaudinis/tp-grupu-darbs">Publiski pieejamais kods (MIT licence)</a></li>
     </ul>
 </footer>
